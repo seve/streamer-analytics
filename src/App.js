1cb934ef960b74f316ba4413c82c1ab14c1eadfb
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-18 14:39:19 -0700

    setup twitch oauth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       id: '',
+       displayName: '',
+       profileImage: ''
+    }
+  }
+  
+
+  componentDidMount = () => {
+    axios.get('https://api.twitch.tv/helix/users')
+      .then((response) => {
+        this.setState({
+          userId: response.id,
+          displayName: response.display_name,
+          profileImage: response.profile_image
+        })
+      })
+  }
+  
   render() {
     return (
       <div className="App">
@@ -19,6 +43,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <a href="https://id.twitch.tv/oauth2/authorize?client_id=lk9hyx24834i7j25guphvmkwvgp86s&redirect_uri=https://c34b75e4.ngrok.io&response_type=token&scope=channel:read:subscriptions">AUTH</a>
+          <h1>User Id: {this.state.userId}</h1>
         </header>
       </div>
     );
