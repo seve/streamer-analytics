3e4b678f56bf42764c0cad8ce4bc1f0f26eb6e9c
Thomas <thomaslee9474@gmail.com>
2019-03-19 15:30:21 -0700

    [update] API queries => async/await

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-import queryString from 'query-string'
+import axios from 'axios';
+import queryString from 'query-string';
+import UserInfo from './UserInfo';
 import './App.css';
-
-import UserInfo from './UserInfo'
-
-
-
 
 class App extends Component {
   constructor(props) {
@@ -19,50 +15,54 @@ class App extends Component {
     }
   }
   
-  
-  componentDidMount = () => {
+  componentDidMount = async () => {
     // GET: USER DATA
     // NEW TWITCH API (MARCH 2019)
-    axios.get('https://api.twitch.tv/helix/users', {
-      // SENDING CLIENT ID TOKEN TO CHECK FOR AUTHENTICATION
-      // HEADER AUTHORISATION WILL BE REQUIRED FOR ALL GET REQUESTS
-      headers: {
-        Authorization: `Bearer ${queryString.parse(document.location.hash).access_token}`
-      }
-      
-    })
-      .then((response) => {
-      if(response){
-        this.setState({
-          userId: response.data.data[0].id,
-          displayName: response.data.data[0].display_name,
-          profileImage: response.data.data[0].profile_image_url
-        })
-      }
-      }).catch((err) => {
-        console.log(err);
-        
-      })
-      .then((hi) => {
-        // GET: SUBSCRIBER COUNT
-        // NEW TWITCH API (MARCH 2019)
-        axios.get(`https://api.twitch.tv/helix/subscriptions?broadcaster_id=${this.state.userId}`, {
+    // MUST BE ARROW FUNCTION TO BIND THIS
+     const getUserData = async () => {
+      try {
+        let userData = await axios.get('https://api.twitch.tv/helix/users', {
+          // SENDING CLIENT ID TOKEN TO CHECK FOR AUTHENTICATION
+          // HEADER AUTHORISATION WILL BE REQUIRED FOR ALL GET REQUESTS
           headers: {
             Authorization: `Bearer ${queryString.parse(document.location.hash).access_token}`
           }
         })
-          .then((response) => {
-            if(response){
-              console.log("hello")
-              console.log(response)
-              return response 
-            }
-          }).catch((err) => {
-            console.log(err)
+
+        if (userData){
+          this.setState({
+            userId: userData.data.data[0].id,
+            displayName: userData.data.data[0].display_name,
+            profileImage: userData.data.data[0].profile_image_url
           })
+        }
+      } catch(error){
+        console.log(error);
+      }
+    }
+
+    // GET: SUBSCRIBER COUNT
+    // NEW TWITCH API (MARCH 2019)
+    const getSubscribers= async () => {
+      try {
+        let subscribers = await axios.get(`https://api.twitch.tv/helix/subscriptions?broadcaster_id=${this.state.userId}`, {
+          headers: {
+            Authorization: `Bearer ${queryString.parse(document.location.hash).access_token}`
+          }
         })
 
-      
+        if (subscribers){
+          console.log("hello")
+          console.log(subscribers)
+          return subscribers 
+        }
+      } catch(error){
+        console.log(error)
+      }
+    }
+
+    await getUserData();
+    getSubscribers();
   }
   
   render() {
---
+++ src/clientinfo.js
@@ -0,0 +1,2 @@
+export const CLIENT_ID='sv7qodeq1ii9rnpnk4jzs5tzmvh036'
+export const REDIRECT_URI='http://736e6d02.ngrok.io'
