de82faf156b3ee0f33c5d6cefee76dde14620c1f
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-20 15:15:55 -0700

    Begin styling with styled components

--- src/UserInfo.js
+++ src/UserInfo.js
@@ -1,20 +1,43 @@
 import React from 'react'
+import styled from 'styled-components/macro'
+
 import logo from './logo.svg'
 import {CLIENT_ID, REDIRECT_URI} from './clientinfo'
 
 
 export default function UserInfo(props) {
-  return (
-    <div>
-      <img style={profileImageStyle} src={props.profileImage ? props.profileImage : logo} className="profile-image" alt="profile"/>
-    {props.id !== '' ?  
-      (<h2>{props.displayName}</h2>) 
-      : (<a href={`https://id.twitch.tv/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=channel:read:subscriptions`}>Log In</a>)}
-    </div>
-  )
-}
+  const Image = styled.img`
+    border-radius: 50%;
+    height: 200px;
+    width: 200px;
+    margin-top: 24px;
+  `
+  const Anchor = styled.a`
+    text-decoration: none;
+  `
+  const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #495264;
+    width: 300px;
+    align-items: center;
+    justify-items: center;
+    border-radius: 8px;
+    -webkit-box-shadow: 0px 9px 23px 1px rgba(0,0,0,0.75);
+    -moz-box-shadow: 0px 9px 23px 1px rgba(0,0,0,0.75);
+    box-shadow: 0px 9px 23px 1px rgba(0,0,0,0.75);
+  `
 
-const profileImageStyle = {
-    borderRadius: '50%',
-    height: '200px',
+  const Name = styled.h2`
+    margin: 16px 0px;
+  `
+  return (
+    
+    <Div>
+      <Image src={props.profileImage ? props.profileImage : logo} className="profile-image" alt="profile"/>
+    {props.userId !== '' ?  
+      (<Name>{props.displayName}</Name>) 
+      : (<Anchor href={`https://id.twitch.tv/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=token&scope=channel:read:subscriptions`}>Log In</Anchor>)}
+    </Div>
+  )
 }
