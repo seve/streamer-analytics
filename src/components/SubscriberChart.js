48de5ee5f64217043510160d0fc5b2f1fdd6cf12
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-24 15:33:24 -0700

    Show subscribers in chart

--- src/components/SubscriberChart.js
+++ src/components/SubscriberChart.js
@@ -19,26 +19,44 @@ const options = {
   }
 }
 
-const dataArray = [['one', 1], ['two', 2], ['three', 3], ['four', 4], ['five', 5]]
-
-const data = {
-  labels: dataArray.map(o => o[0]),
-  datasets: [{
-    fill: true,
-    data: dataArray.map(o => o[1]),
-    backgroundColor: '#495264',
-    borderJoinStyle: 'miter',
-
-  }]
-}
-
 const mapStateToProps = (state) => {
   return {
-    user: state.user 
+    user: state.user,
+    subscribers: state.subscribers
   }
 }
 
 export default connect( mapStateToProps, { fetchSubscribers })( class SubscriberChart extends Component {
+  data = {
+    labels: [],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: []
+        // data: [0, 330]
+      }
+    ]
+  };
+
+
+  
 
   mapDispatchToProps = () => {
     return {
@@ -48,13 +66,24 @@ export default connect( mapStateToProps, { fetchSubscribers })( class Subscriber
 
   componentWillMount = () => {
     this.props.fetchSubscribers();
-    
+    setInterval(() => {
+      this.props.fetchSubscribers()
+    }, 10000)
+  }
+
+  componentWillReceiveProps = (nextProps) => {
+    this.data.datasets[0].data = nextProps.subscribers.users.slice(1).map( x => x.length)
+    if (this.data.datasets[0].data.length > 0){
+      this.data.labels.push(new Date().toUTCString().substring(18))
+
+    }
   }
   
   render() {
+    
     return (
       <Chart>
-        <Line options={options} data={data}/>
+        <Line options={options} data={this.data} ref={(ref) => this.chart = ref }/>
       </Chart>
     )
   }
