84d42264997114e97acd708eca4b840d1fa599a3
Severiano Badajoz <sevebadajoz@gmail.com>
2019-03-24 19:28:20 -0700

    Update colors

--- src/colors.js
+++ src/colors.js
@@ -1,9 +1,10 @@
 /* Coolors Exported Palette - coolors.co/de6b48-5eb1bf-e5b181-e0a890-f4b9b2 */
 
 export const  COLORS = {
-  RED: '#DE6B48',
-  BLUE:  '#5EB1BF',
-  ORANGE: '#DE6B48',
-  TAN: '#DE6B48',
-  PINK: '#DE6B48'
+  RED: '#995C4C',
+  DARK_BLUE: '#36667C',
+  BLUE:  '#659188',
+  LIGHT_BLUE: '#4C87A2',
+  LAVENDER: '#9B919C',
+  GREY: '#9A9897'
 }
